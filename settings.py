9a929c25c725be971d220aebd4ff27fcb96e473e
Arjen Holmer <arjen@holmer.tk>
2015-07-24 18:20:50 +0200

    Changed setting for testing

--- settings.py
+++ settings.py
@@ -5,8 +5,8 @@ __author__ = 'Arjen'
 
 
 # Database connection:
-db_file = 'database/db.sqlite'
-db_con = 'sqlite:///%s' % db_file
+db_file = 'alchtest:alchtest@localhost:5432/alchtest'
+db_con = 'postgresql://%s' % db_file
 
 # Echo database queries:
-db_echo = True
+db_echo = False
